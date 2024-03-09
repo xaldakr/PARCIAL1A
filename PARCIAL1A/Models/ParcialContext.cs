095dd11a99862aaa3663a8171dc54f8880cd7148
xaldakr <xavieravilaposada@gmail.com>
2024-03-09 12:07:58 -0600

    Agregar archivos de proyecto.

---
+++ PARCIAL1A/Models/ParcialContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace PARCIAL1A.Models
+{
+    public class ParcialContext : DbContext
+    {
+        public ParcialContext(DbContextOptions<ParcialContext> options) : base(options)
+        {
+
+        }
+        
+    }
+}
+
