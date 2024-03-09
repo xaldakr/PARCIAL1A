7ea4026003decd7a533da5b612a589ad159a2142
xaldakr <xavieravilaposada@gmail.com>
2024-03-09 12:35:14 -0600

    Dos controller mas

---
+++ PARCIAL1A/Controllers/AutorController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PARCIAL1A.Models;
+
+namespace PARCIAL1A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+        private readonly ParcialContext _ParcialContexto;
+
+        public AutorController(ParcialContext ParcialContexto)
+        {
+            _ParcialContexto = ParcialContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Getto()
+        {
+            List<autor> listadoautor = (from e in _ParcialContexto.Autores
+                                      select e).ToList();
+            if (listadoautor.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoautor);
+        }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(int id)
+        {
+            autor? autor = (from e in _ParcialContexto.Autores
+                            where e.Id == id
+                          select e).FirstOrDefault();
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return Ok(autor);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Guardarautor([FromBody] autor autor)
+        {
+            try
+            {
+                _ParcialContexto.Autores.Add(autor);
+                _ParcialContexto.SaveChanges();
+                return Ok(autor);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Actualizar/{id}")]
+        public IActionResult Actualizarautor(int id, [FromBody] autor autorModificar)
+        {
+            //Obtener dato actual
+            autor? autorActual = (from e in _ParcialContexto.Autores
+                                  where e.Id == id
+                                select e).FirstOrDefault();
+            //Verificar su existencia
+            if (autorActual == null)
+            {
+                return NotFound();
+            }
+            //Si se encuentra, modificar
+            autorActual.Nombre = autorModificar.Nombre;
+            //Marcando como modificado
+            _ParcialContexto.Entry(autorActual).State = EntityState.Modified;
+            _ParcialContexto.SaveChanges();
+
+            return Ok(autorModificar);
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id}")]
+        public IActionResult Eliminarautor(int id)
+        {
+            autor? autor = (from e in _ParcialContexto.Autores
+                            where e.Id == id
+                          select e).FirstOrDefault();
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            _ParcialContexto.Autores.Attach(autor);
+            _ParcialContexto.Autores.Remove(autor);
+            _ParcialContexto.SaveChanges();
+            return Ok(autor);
+        }
+    }
+}
--- PARCIAL1A/Controllers/PostController.cs
+++ PARCIAL1A/Controllers/PostController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using PARCIAL1A.Models;
 
 namespace PARCIAL1A.Controllers
@@ -9,6 +10,96 @@ namespace PARCIAL1A.Controllers
     [ApiController]
     public class PostController : ControllerBase
     {
-        
+        private readonly ParcialContext _ParcialContexto;
+
+        public PostController(ParcialContext ParcialContexto)
+        {
+            _ParcialContexto = ParcialContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Getto()
+        {
+            List<post> listadopost = (from e in _ParcialContexto.Posts
+                                      select e).ToList();
+            if (listadopost.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadopost);
+        }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(int id)
+        {
+            post? post = (from e in _ParcialContexto.Posts
+                          where e.Id == id
+                           select e).FirstOrDefault();
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Guardarpost([FromBody] post post)
+        {
+            try
+            {
+                _ParcialContexto.Posts.Add(post);
+                _ParcialContexto.SaveChanges();
+                return Ok(post);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Actualizar/{id}")]
+        public IActionResult Actualizarpost(int id, [FromBody] post postModificar)
+        {
+            //Obtener dato actual
+            post? postActual = (from e in _ParcialContexto.Posts
+                                where e.Id == id
+                                 select e).FirstOrDefault();
+            //Verificar su existencia
+            if (postActual == null)
+            {
+                return NotFound();
+            }
+            //Si se encuentra, modificar
+            postActual.Titulo = postModificar.Titulo;
+            postActual.Contenido = postModificar.Contenido;
+            postActual.FechaPublicacion = postModificar.FechaPublicacion;
+            postActual.AutorId = postModificar.AutorId;
+            //Marcando como modificado
+            _ParcialContexto.Entry(postActual).State = EntityState.Modified;
+            _ParcialContexto.SaveChanges();
+
+            return Ok(postModificar);
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id}")]
+        public IActionResult Eliminarpost(int id)
+        {
+            post? post = (from e in _ParcialContexto.Posts
+                          where e.Id == id
+                           select e).FirstOrDefault();
+            if (post == null)
+            {
+                return NotFound();
+            }
+            _ParcialContexto.Posts.Attach(post);
+            _ParcialContexto.Posts.Remove(post);
+            _ParcialContexto.SaveChanges();
+            return Ok(post);
+        }
     }
 }
